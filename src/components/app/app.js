1a960de91850b89a33302a3540b6e4ac43b153e0
Alexandra Khutorna <khutornaalexandra@gmail.com>
2021-02-05 16:06:05 +0200

    Writing application logic

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,27 +1,26 @@
 import React from 'react';
 import {MainPage, CartPage} from '../pages';
 import AppHeader from '../app-header';
-import WithRestoService from '../hoc';
 import {Route, Switch} from 'react-router-dom';
 
 import Background from './food-bg.jpg';
 
-const App = ({RestoService}) => {
-
-    RestoService.getMenuItems()
-    .then( menu => console.log(menu))
-    .catch(error => {console.log('Произошла ошибка')})
+const App = () => {
 
     return (
         <div style={{background: `url(${Background}) center center/cover no-repeat`}} className="app">
             <AppHeader total={50}/>
             <Switch>
-                <Route path='/' exact component={MainPage}/>
-                <Route path = '/cart' component={CartPage}/>
-                <Route  exact component={MainPage}/>
+                <Route 
+                    path='/' 
+                    component={MainPage}
+                    exact/>
+                <Route 
+                    path = '/cart' 
+                    component={CartPage}/>
             </Switch>
         </div>
     )
 }
 
-export default WithRestoService()(App); //чтобы можно было использовать контекст
+export default App; 
